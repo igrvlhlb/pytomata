e07ec2b146baac4b6fda116bc8675c3a1157edb1
Igor de Araujo Vilhalba <igor.vilhalba@gmail.com>
2021-05-21 03:27:49 -0300

    Adds classes for exceptions

--- automaton.py
+++ automaton.py
@@ -9,3 +9,22 @@ class Automaton:
     def __str__(self):
         return '(Alphabet: {}, States: {}, Initial State: {}, Final States: {}, Transitions: {})'\
         .format(self.alphabet, self.states, self.initial_state, self.final_states, self.transitions)
+    
+    def __repr__(self):
+        return self.__str__()
+    
+class InvalidStateError(Exception):
+    def __init__(self, state):
+        self.__state = state
+    def __str__(self):
+        return '{} is not a valid state name'.format(self.__state)
+
+class InvalidSymbolError(Exception):
+    def __init__(self, symbol):
+        self.__symbol = symbol
+    def __str__(self):
+        return '{} does not belong to the input alphabet'.format(self.__symbol)
+
+class InvalidStackSymbolError(InvalidSymbolError):
+    def __str__(self):
+        return super.__str__().replace('input', 'stack')
