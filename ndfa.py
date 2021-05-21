1f6e552e3aaa2b332fa024f0d3c4ef0e5be18772
Igor de Araujo Vilhalba <igor.vilhalba@gmail.com>
2021-05-21 00:16:56 -0300

    Implements the is_valid() method

--- ndfa.py
+++ ndfa.py
@@ -1,11 +1,9 @@
-class NDFA:
+from automaton import Automaton
+
+class NDFA(Automaton):
 
     def __init__(self, alphabet, states, initial_state, final_states, transitions):
-        self.alphabet = set(alphabet)
-        self.states = set(states)
-        self.initial_state = initial_state
-        self.final_states = set(final_states)
-        self.transitions = transitions
+        super.__init__(alphabet,states,initial_state,final_states,transitions)
 
     def transition(self, state, symbol):
         if self.transitions.get(state) is not None:
@@ -13,7 +11,7 @@ class NDFA:
                 return self.transitions[state][symbol]
         return None
 
-    def read(self, input):
+    def read(self, input, show_steps=False):
 
         current_states = [self.initial_state]
         syms = [sym for sym in input]  # separate the input symbols
@@ -21,7 +19,8 @@ class NDFA:
 
         while len(syms) != 0:
             next_states = []
-            self.show_config(clock, current_states,''.join(syms))
+            if show_steps is True:
+                self._show_config(clock, current_states,''.join(syms))
             for state in current_states:
                 # make all the epsilon-transitions
                 epsT = self.transition(state, '&')
@@ -54,25 +53,40 @@ class NDFA:
                     for s in epsT:
                         if s not in current_states:
                             current_states.append(s)
-        
-        self.show_config(clock, current_states,''.join(syms))
+        if show_steps is True:
+            self._show_config(clock, current_states,''.join(syms))
         return (''.join(syms), current_states)
 
     def accepts(self, input):
         remaining_input, states = self.read(input)
         reached_finals = self.final_states.intersection(set(states))
-        print(reached_finals)
         if len(remaining_input) == 0 and len(reached_finals) != 0:
             return True
         else:
             return False
 
-    def __str__(self):
-        return '(Alphabet: {}, States: {}, Initial State: {}, Final States: {}, Transitions: {})'\
-        .format(self.alphabet, self.states, self.initial_state, self.final_states, self.transitions)
-    
-    def show_config(self, clock, states, input):
+    def _show_config(self, clock, states, input):
         print('Clock: {}, Current States: {}, Remaining Input: \'{}\''.format(clock, states, input))
 
-    def validate(self):
-        # TODO
+    # return True if the automaton
+    # is valid or False otherwise
+    def is_valid(self):
+        # '&' is a special symbol
+        # that denotes epsilon
+        for token in self.alphabet\
+            .union(self.states)\
+            .union({self.initial_state})\
+            .union(self.final_states):
+            if len(token) == 0 or token.isspace() or token == '&':
+                print("Falhou primeiro for")
+                return False
+        for state, transition in self.transitions.items():
+            if state not in self.states:
+                return False
+            for symbol, states in transition.items():
+                if symbol not in self.alphabet and symbol != '&'\
+                    or len(set(states).difference(self.states)) != 0:
+                    print(self.alphabet, symbol, set(states).difference(self.states))
+                    print("Falhou Segundo For")
+                    return False
+        return True
